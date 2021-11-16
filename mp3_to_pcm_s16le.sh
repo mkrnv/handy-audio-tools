01b416ed6891e693d6c79aabcee199a7e98f72ea
dmitrijthedaddy <demitya7@gmail.com>
2021-11-16 14:32:11 +0500

    turn off ffmpeg logging & add echoes for each loop iteration

--- mp3_to_pcm_s16le.sh
+++ mp3_to_pcm_s16le.sh
@@ -3,7 +3,8 @@
 mkdir wav
 for f in *.mp3
 do
-    ffmpeg -i "$f" -acodec pcm_s16le -ac 2 -ar 44100 "wav/$(basename "$f" .mp3).wav"
+    ffmpeg -i "$f" -acodec pcm_s16le -ac 2 -ar 44100 "wav/$(basename "$f" .mp3).wav" -loglevel quiet
+    echo "[mp3 * -> wav pcm_s16le] Converted: $f"
 done
 if [ $? -eq 0 ]; then
     echo "Conversion complete"
--- wav_to_pcm_s16le.sh
+++ wav_to_pcm_s16le.sh
@@ -3,7 +3,8 @@
 mkdir wav
 for f in *.wav
 do
-    ffmpeg -i "$f" -acodec pcm_s16le -ac 2 -ar 44100 "wav/$f"
+    ffmpeg -i "$f" -acodec pcm_s16le -ac 2 -ar 44100 "wav/$f" -loglevel quiet
+    echo "[wav * -> wav pcm_s16le] Converted: $f"
 done
 if [ $? -eq 0 ]; then
     echo "Conversion complete"
